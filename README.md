5a1f377a7c6c7146e64996ae0ad9179be9209a41
Thorsten Lorenz <thlorenz@gmx.de>
2013-11-19 20:38:35 -0500

    ex16 without using malloc, but freeing fails

---
+++ README.md
@@ -0,0 +1,4 @@
+learn-c-the-hard-way
+====================
+
+Samples from learn C the hard way
---
+++ ex16_structs_and_pointers_nomalloc.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person {
+  char *name;
+  int age;
+  int height;
+  int weight;
+};
+
+struct Person Person_new(char *name, int age, int height, int weight) {
+  struct Person who;
+
+  who.name   =  strdup(name);
+  who.age    =  age;
+  who.height =  height;
+  who.weight =  weight;
+
+  return who;
+}
+
+void Person_free(struct Person *who) {
+  assert(who != NULL);
+  free(who->name);
+  free(who);
+}
+
+void Person_print(struct Person who) {
+  printf("Name: %s\n"     ,  who.name);
+  printf("\tAge: %d\n"    ,  who.age);
+  printf("\tHeight: %d\n" ,  who.height);
+  printf("\tWeight: %d\n" ,  who.weight);
+}
+
+int main(void)
+{
+  struct Person joe   =  Person_new("Joe Alex", 32, 64, 14);
+  struct Person frank =  Person_new("Frank Blank", 20, 72, 180);
+
+  printf("Joe is stored at %p\n", &joe);
+  Person_print(joe);
+
+  printf("Frank is stored at %p\n", &frank);
+  Person_print(frank);
+
+  joe.age += 20;
+  Person_print(joe);
+
+  frank.age += 10;
+  Person_print(frank);
+
+  // Wondering why this works up to here at all if no memory was allocated for the struct??
+
+  // => malloc: *** error for object 0x7fff5f401880: pointer being freed was not allocated
+  Person_free(&joe);
+  Person_free(&frank);
+
+  return 0;
+}
