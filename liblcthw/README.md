dafd7f77bdd8bdbe32a7f61a0e7b051bdae160c9
Thorsten Lorenz <thlorenz@gmx.de>
2013-12-10 09:20:46 -0500

    linked list implementation inside lcthw lib

---
+++ liblcthw/README.md
@@ -0,0 +1,8 @@
+# liblcthw
+
+Library built in exercises 32-47 of [Learn C the hard Way](http://c.learncodethehardway.org/book/).
+
+Although I understand that traditionally C libraries contain *lots of features*, IMO it would be better to create a
+separate library for each concern, i.e. a linked list lib, a radix map lib, etc.
+
+That way I can install and include exactly what I want, not a huge swiss army knife just because I need a toothpick.
---
+++ liblcthw/src/lcthw/list.c
@@ -0,0 +1,113 @@
+#include <lcthw/list.h>
+#include <lcthw/dbg.h>
+
+List *List_create() {
+  return calloc(1, sizeof(List));
+}
+
+void List_destroy(List *list) {
+  LIST_FOREACH(list, first, next, cur) {
+    if (cur->prev) free(cur->prev);
+  }
+
+  free(list->last);
+  free(list);
+}
+
+void List_clear(List *list) {
+  LIST_FOREACH(list, first, next, cur) {
+    free(cur->value);
+  }
+}
+
+void List_clear_destroy(List *list) {
+  List_clear(list);
+  List_destroy(list);
+}
+
+void List_push(List *list, void *value) {
+  ListNode *node = calloc(1, sizeof(ListNode));
+  check_mem(node);
+
+  node->value = value;
+
+  if (list->last == NULL) {
+    list->first = node;
+    list->last = node;
+  } else {
+    list->last->next = node;
+    node->prev = list->last;
+    list->last = node;
+  }
+
+  list->count++;
+
+error:
+  return;
+}
+
+void *List_pop(List *list) {
+  ListNode *node = list->last;
+  return node != NULL ? List_remove(list, node) : NULL;
+}
+
+void List_unshift(List *list, void *value) {
+  ListNode *node = calloc(1, sizeof(ListNode));
+  check_mem(node);
+
+  node->value = value;
+
+  if (list->first == NULL) {
+    list->first = node;
+    list->last = node;
+  } else {
+    list->first->prev = node;
+    node->next = list->first;
+    list->first = node;
+  }
+
+  list->count++;
+
+error:
+  return;
+}
+
+void *List_shift(List *list) {
+  ListNode *node = list->first;
+  return node != NULL ? List_remove(list, node) : NULL;
+}
+
+void *List_remove(List *list, ListNode *node) {
+  void *result = NULL;
+  check(list->first && list->last, "List is empty");
+  check(node, "node can't be NULL");
+
+  if (node == list->first && node == list->last) {
+    // last node left
+    list->first = NULL;
+    list->last = NULL;
+  } else if (node == list->first) {
+    // first node
+    list->first = node->next;
+    check(list->first != NULL, "Invalid list, second node is NULL");
+    list->first->prev = NULL;
+  } else if (node == list->last) {
+    // last node
+    list->last = node->prev;
+    check(list->last != NULL, "Invalid list, second to last node is NULL");
+    list->last->next = NULL;
+  } else {
+    // node in the middle
+    ListNode *after = node->next;
+    ListNode *before = node->prev;
+    after->prev = before;
+    before->next = after;
+  }
+
+  list->count--;
+  result = node->value;
+  free(node);
+
+error:
+  return result;
+}
